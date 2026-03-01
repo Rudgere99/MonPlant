6d2f28f54142c2fa5ae7fd039816502781a7e561
Rudgere99 <rudgeregermanos@gmail.com>
2026-03-01 08:39:41 -0300

    Update isMobile.ts

--- frontend/src/mobile/isMobile.ts
+++ frontend/src/mobile/isMobile.ts
@@ -1,3 +1,5 @@
+// src/mobile/isMobile.ts
 export function isMobileViewport() {
-  return window.matchMedia('(max-width: 900px)').matches;
+  if (typeof window === "undefined") return false;
+  return window.matchMedia("(max-width: 860px)").matches;
 }
