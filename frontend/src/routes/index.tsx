47829fb22bb574c96d2f906a7e2f16253e9a9503
Rudgere99 <rudgeregermanos@gmail.com>
2026-03-04 16:38:49 -0300

    Update index.tsx

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -15,7 +15,7 @@ import PlantProduction from "../pages/PlantProduction";
 import Ritmo from "../pages/Ritmo";
 import Statistics from "../pages/Statistics";
 import UfDF from "../pages/UfDF";
-import AbastecimentoBT01 from "../pages/Abastecimento
+import AbastecimentoBT01 from "../pages/Abastecimento";
 
 import Horimetros from "../pages/Horimetros";
 import Paradas from "../pages/Paradas";
